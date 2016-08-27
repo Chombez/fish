576fd7dd43be1ef31f56c1d794325406a73199cc
Ryan Dicembre <ryandicembre@gmail.com>
2016-08-27 19:16:49 +1000

    Add sorry function

---
+++ functions/sorry.fish
@@ -0,0 +1,6 @@
+function sorry --description 'Replace the first word in the last command with a new one'
+	set -l lastCommand $history[1]
+	set -l args (echo $lastCommand |  sed -r "s/^(\w+)//")
+	eval $argv $args
+end
+
