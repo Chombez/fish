5e69561cb719bae2bc6d121d21cc9e983bb9bffe
Ryan Dicembre <ryandicembre@gmail.com>
2016-09-05 21:03:04 +1000

    Add tmux config folder to check function

--- functions/check_config_status.fish
+++ functions/check_config_status.fish
@@ -1,6 +1,6 @@
 function check_config_status
 	set -l __starting_dir (pwd)
-	set -l config_dirs ~/.vim ~/.config/fish ~/.config/git
+	set -l config_dirs ~/.vim ~/.config/fish ~/.config/git ~/.config/tmux
     for path in $config_dirs
         cd $path
         echo $path | grep -o '[^/]*$'
